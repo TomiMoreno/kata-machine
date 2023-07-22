a9cd32c9a76ea918e707cbb5e041aad2ac8dd23b
TomiMoreno <tomimoreno03@gmail.com>
2023-07-22 02:37:55 -0300

    Binary search implementation

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,14 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let high = haystack.length,
+        low = 0;
 
+    while (low < high) {
+        const midIndex = Math.floor(low + (high - low) / 2);
+        const value = haystack[midIndex];
+        if (value === needle) return true;
+        if (value < needle) low = midIndex + 1;
+        else high = midIndex;
+    }
+
+    return false;
 }
