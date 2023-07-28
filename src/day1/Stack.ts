5a91ae1135a925501073323bb0c1ca588624e8c2
TomiMoreno <tomimoreno03@gmail.com>
2023-07-28 12:11:46 -0300

    Add stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,33 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
     push(item: T): void {
+        this.length++;
+        const head = this.head;
+        const node = { value: item, prev: head };
 
-}
+        this.head = node;
+    }
     pop(): T | undefined {
+        if (!this.head) return undefined;
+        this.length--;
+        const head = this.head;
+        this.head = head.prev;
 
-}
+        return head.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
